f8bdb5a5d2c9fa7365537c80c07e1dffa3dbcc95
Amy Westcott <amy.westcott@yahoo.com>
2019-07-09 22:34:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,4 +5,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  
+  projects
 end
